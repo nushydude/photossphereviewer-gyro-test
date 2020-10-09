9ae6bfc2abddc83856dd037188be1a22daaa82af
Anusha Dharmasena <anusha@lithodomosvr.com>
2020-10-09 19:39:25 +1100

    set a FOV changer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 // import './threeModules';
-import './photo-sphere-viewer.css';
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components'; 
-import { isIOS, isMobileSafari, isSafari, isIPad13, isMobile } from 'react-device-detect';
-import * as PhotoSphereViewer from 'photo-sphere-viewer';
+import "./photo-sphere-viewer.css";
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import {
+  isIOS,
+  isMobileSafari,
+  isSafari,
+  isIPad13,
+  isMobile,
+} from "react-device-detect";
+import * as PhotoSphereViewer from "photo-sphere-viewer";
 import GyroscopePlugin from "photo-sphere-viewer/dist/plugins/gyroscope";
-import eruda from 'eruda';
-import panorama from './assets/f4399f2b0b4bd8ba8406908b798add0b.jpg';
-import { OrientationListener } from './OrientationListener';
-import { getCompassHeadingExtra } from './compassHeading';
+import eruda from "eruda";
+import panorama from "./assets/f4399f2b0b4bd8ba8406908b798add0b.jpg";
+import { OrientationListener } from "./OrientationListener";
+import { getCompassHeadingExtra } from "./compassHeading";
 
 eruda.init({
-  tool: ['console', 'elements'],
+  tool: ["console", "elements"],
   useShadowDom: true,
   autoScale: true,
   defaults: {
     displaySize: 50,
     transparency: 0.9,
-    theme: 'Monokai Pro',
+    theme: "Monokai Pro",
   },
 });
 
 const defaultPanoOptions = {
   autorotateDelay: false,
-  autorotateSpeed: '60dpm',
+  autorotateSpeed: "60dpm",
   autorotateLat: 0,
   navbar: false,
-  minFov: 40,
-  maxFov: 80,
+  minFov: 1,
+  maxFov: 179,
 };
 
 class App extends React.Component {
@@ -37,34 +43,31 @@ class App extends React.Component {
     this.initPhotoSphere();
   }
 
-
   initPhotoSphere = () => {
     const options = {
       ...defaultPanoOptions,
-      container: document.querySelector('#photosphere'),
+      container: document.querySelector("#photosphere"),
       panorama,
-      defaultZoomLvl: 60,
+      defaultZoomLvl: 80,
       defaultLat: 0,
       defaultLong: 0,
       size: {
         width: window.innerWidth,
         height: window.innerHeight,
       },
-      navbar: ['autorotate'],
-      plugins: [
-        GyroscopePlugin,
-      ],
+      navbar: ["autorotate"],
+      plugins: [GyroscopePlugin],
     };
 
     this.photoSphereViewer = new PhotoSphereViewer.Viewer(options);
 
-    this.photoSphereViewer.on('ready', () => {
+    this.photoSphereViewer.on("ready", () => {
       // window.addEventListener('resize', this.onResize, false);
     });
   };
 
   onResize = () => {
-    const photoSphereElem = document.querySelector('#photosphere');
+    const photoSphereElem = document.querySelector("#photosphere");
 
     if (!photoSphereElem) {
       return;
@@ -78,8 +81,8 @@ class App extends React.Component {
     photoSphereElem.style.height = `100%`;
 
     this.photoSphereViewer.resize({
-      width: '100%',
-      height: '100%',
+      width: "100%",
+      height: "100%",
     });
 
     this.photoSphereViewer._onResize();
@@ -92,23 +95,26 @@ class App extends React.Component {
 
     const plugin = this.photoSphereViewer.getPlugin(GyroscopePlugin);
 
-    if (plugin && typeof plugin.isEnabled !==  'function') {
-      console.log('plugin issue');
+    if (plugin && typeof plugin.isEnabled !== "function") {
+      console.log("plugin issue");
     }
 
     if (plugin.isEnabled()) {
       plugin.stop();
     } else {
-      plugin.start().then(() => {        
-        const { latitude } = this.photoSphereViewer.getPosition();
-        const { heading } = getCompassHeadingExtra();
+      plugin
+        .start()
+        .then(() => {
+          const { latitude } = this.photoSphereViewer.getPosition();
+          const { heading } = getCompassHeadingExtra();
 
-        this.photoSphereViewer.rotate({
-          longitude: degreesToRadians(heading),
-          latitude,
-        });
-      }).catch(this.handleGyroEnableError);
-    }      
+          this.photoSphereViewer.rotate({
+            longitude: degreesToRadians(heading),
+            latitude,
+          });
+        })
+        .catch(this.handleGyroEnableError);
+    }
   };
 
   handleGyroEnableError = () => {
@@ -117,30 +123,27 @@ class App extends React.Component {
         // @ts-ignore
         window.DeviceOrientationEvent !== undefined &&
         // @ts-ignore
-        typeof window.DeviceOrientationEvent.requestPermission ===
-          'function'
+        typeof window.DeviceOrientationEvent.requestPermission === "function"
       ) {
         // @ts-ignore
-        window.DeviceOrientationEvent.requestPermission().then(
-          (response) => {
-            if (response !== 'granted') {
-              this.showGyroErrorAlert(
-                'Gyroscope permissions have been denied. Please clear the website data from Settings -> Safari -> Advanced -> Website Data, then make sure to refresh the page and tap the Gyroscope icon.'
-              );
-            } else {
-              window.location.reload();
-            }
+        window.DeviceOrientationEvent.requestPermission().then((response) => {
+          if (response !== "granted") {
+            this.showGyroErrorAlert(
+              "Gyroscope permissions have been denied. Please clear the website data from Settings -> Safari -> Advanced -> Website Data, then make sure to refresh the page and tap the Gyroscope icon."
+            );
+          } else {
+            window.location.reload();
           }
-        );
+        });
       } else {
         this.showGyroErrorAlert(
-          'Please enable Motion & Orientation Access from Settings -> Safari, then make sure to refresh the page and tap the Gyroscope icon.'
+          "Please enable Motion & Orientation Access from Settings -> Safari, then make sure to refresh the page and tap the Gyroscope icon."
         );
       }
     } else {
-      this.showGyroErrorAlert('The Gyroscope is not supported on this device.');
+      this.showGyroErrorAlert("The Gyroscope is not supported on this device.");
     }
-  }
+  };
 
   showGyroErrorAlert = (message: string) => {
     window.alert(message);
@@ -149,7 +152,7 @@ class App extends React.Component {
   cleanup = () => {
     if (this.photoSphereViewer) {
       // TODO: Firefox has an issue, so wrapping in a try catch block.
-      try {        
+      try {
         if (this.state.gyroEnabled) {
           this.photoSphereViewer.stopGyroscopeControl();
         }
@@ -170,6 +173,33 @@ class App extends React.Component {
   componentWillUnmount(): void {
     this.cleanup();
   }
+
+  updateFOV = (e) => {
+    if (!this.photoSphereViewer) {
+      return;
+    }
+
+    const fovStr = e.target.value;
+
+    try {
+      const fov = parseFloat(fovStr);
+
+      console.log("fov:", fov);
+
+      if (fov < 1 || fov > 179) {
+        window.alert("set between 1 and 179");
+        return;
+      }
+
+      const { minFov, maxFov } = defaultPanoOptions;
+
+      const zoom = ((maxFov - fov) * 100) / (maxFov - minFov);
+
+      console.log("zoom:", zoom);
+
+      this.photoSphereViewer.zoom(zoom);
+    } catch (error) {}
+  };
 
   render() {
     return (
@@ -198,7 +228,7 @@ class App extends React.Component {
           <Button
             onClick={() => {
               if (this.photoSphereViewer) {
-                this.toggleGyroscopeControl();             
+                this.toggleGyroscopeControl();
               }
             }}
           >
@@ -206,7 +236,12 @@ class App extends React.Component {
           </Button>
         </Buttons>
 
-      {isMobile && <OrientationListener />}
+        <FOVChangerContainer>
+          <p>Set FOV (degrees)</p>
+          <input type="number" onChange={this.updateFOV} min={1} max={179} />
+        </FOVChangerContainer>
+
+        {isMobile && <OrientationListener />}
       </>
     );
   }
@@ -243,16 +278,24 @@ const RendererContainer = styled.div`
   overflow: hidden;
 `;
 
-
 const Buttons = styled.div`
   position: fixed;
   z-index: 2;
   top: 30px;
   left: 30px;
   display: grid;
-  grid-gap: 8px;  
+  grid-gap: 8px;
 `;
 
-const Button =  styled.button`
+const FOVChangerContainer = styled.div`
+  position: fixed;
+  z-index: 2;
+  top: 150px;
+  left: 30px;
+  display: grid;
+  grid-gap: 8px;
+`;
+
+const Button = styled.button`
   padding: 20px;
 `;
